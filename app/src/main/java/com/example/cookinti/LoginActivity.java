2c590713ecda0472c3005866036b3d28edc28009
Menuoo <lietuvis7080@gmail.com>
2026-03-16 09:46:12 +0200

    Update LoginActivity.java

--- app/src/main/java/com/example/cookinti/LoginActivity.java
+++ app/src/main/java/com/example/cookinti/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.cookinti;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
