56753888cbc5313045ae51a4c9e0693074753557
Menuoo <lietuvis7080@gmail.com>
2026-03-18 21:22:01 +0200

    single recipe view updated
    -- back button addded
    -- now displays ingredients and cooking steps (FROM DATABASE) in addition to the standard feed display

--- app/src/main/java/com/example/cookinti/RecipeActivity.java
+++ app/src/main/java/com/example/cookinti/RecipeActivity.java
@@ -1,13 +1,25 @@
 package com.example.cookinti;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -36,5 +48,51 @@ public class RecipeActivity extends AppCompatActivity {
 
         String userName = db.userDao().getUser(rec.getFk_userid()).getUsername();
         recipeHolder.getAuthorText().setText(userName);
+
+
+        //String[] ingredients = {"Salota", "Morka", "Bananas", "Cepelinai"};
+        LinearLayout ingredientLayout = findViewById(R.id.ingredientLayout);
+        LinearLayout stepLayout = findViewById(R.id.stepsLayout);
+
+        Log.d("JSON", rec.getIngredients());
+
+        PopulateLayout(rec.getIngredients(), ingredientLayout);
+        PopulateLayout(rec.getSteps(), stepLayout);
+
+        Button back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    void PopulateLayout(String jsonString, LinearLayout layout)
+    {
+        JSONArray array;
+        try {
+            array = new JSONArray(jsonString);
+            array = new JSONArray(array.toString()); // galima automatizuoti
+
+            Log.d("JSON", array.toString());
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < array.length(); i++) {
+            Object row = null;
+            try {
+                row = array.get(i);
+
+                TextView newText = new TextView(this);
+                newText.setText(row.toString());
+                layout.addView(newText);
+
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
